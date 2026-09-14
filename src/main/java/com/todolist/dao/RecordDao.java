dd4a77bd104e3ea9ce12702b531203f28be3046d
vladizxc <vlad08052@gmx.de>
2026-09-14 17:35:29 +0200

    added dao for records

---
+++ src/main/java/com/todolist/dao/RecordDao.java
@@ -0,0 +1,23 @@
+package com.todolist.dao;
+
+import com.todolist.entity.Record;
+import com.todolist.entity.RecordStatus;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class RecordDao {
+    private final List<Record> records = new ArrayList<>(
+            Arrays.asList(
+                    new Record("Take a shower", RecordStatus.ACTIVE),
+                    new Record("Buy flowers", RecordStatus.DONE),
+                    new Record("Go to the gym", RecordStatus.ACTIVE)
+            ));
+
+    public List<Record> findALlRecords(){
+        return new ArrayList<>(records);
+    }
+}
