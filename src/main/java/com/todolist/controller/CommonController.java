21fb27e786ed4ca05c9cf133195af4c1bf26ef02
vladizxc <vlad08052@gmx.de>
2026-09-14 18:04:03 +0200

    added record service to the common controller to show all records

--- src/main/java/com/todolist/controller/CommonController.java
+++ src/main/java/com/todolist/controller/CommonController.java
@@ -1,13 +1,32 @@
 package com.todolist.controller;
 
+import com.todolist.entity.Record;
+import com.todolist.entity.RecordStatus;
+import com.todolist.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class CommonController {
 
+    private final RecordService recordService;
+
+    @Autowired
+    public CommonController(RecordService recordService){
+        this.recordService = recordService;
+    }
+
     @RequestMapping("/home")
-    public String getMainPage(){
+    public String getMainPage(Model model){
+        List<Record> records = recordService.findAllRecords();
+        int numberOfActiveRecords = (int) records.stream().filter(record -> record.getStatus() == RecordStatus.ACTIVE).count();
+        int numberOfDoneRecords = (int) records.stream().filter(record -> record.getStatus() == RecordStatus.DONE).count();
+        model.addAttribute("numberOfDoneRecords", numberOfDoneRecords);
+        model.addAttribute("numberOfActiveRecords", numberOfActiveRecords);
         return "main-page";
     }
 }
