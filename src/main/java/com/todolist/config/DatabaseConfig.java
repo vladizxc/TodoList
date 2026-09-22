79262edda49b6e843ea0e16281de296c48000177
vladizxc <vlad08052@gmx.de>
2026-09-22 18:11:18 +0200

    created DatabaseConfig and updated record entity

---
+++ src/main/java/com/todolist/config/DatabaseConfig.java
@@ -0,0 +1,66 @@
+package com.todolist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DatabaseConfig {
+
+    @Value("${db.driver}")
+    private String databaseDriver;
+
+    @Value("${db.url}")
+    private String databaseUrl;
+
+    @Value("${db.username}")
+    private String databaseUsername;
+
+    @Value("${db.password}")
+    private String databasePassword;
+
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
+
+    @Value("${hibernate.show_sql}")
+    private String hibernateShowSql;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hibernateHbm2DdlAuto;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(databaseDriver);
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(databaseUsername);
+        dataSource.setPassword(databasePassword);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setPackagesToScan("com.todolist.entity");
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory.setJpaProperties(getHibernateProperties());
+        return entityManagerFactory;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", hibernateDialect);
+        properties.setProperty("hibernate.show_sql", hibernateShowSql);
+        properties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2DdlAuto);
+        return properties;
+    }
+}
--- src/main/java/com/todolist/dao/RecordDao.java
+++ src/main/java/com/todolist/dao/RecordDao.java
@@ -2,23 +2,47 @@ package com.todolist.dao;
 
 import com.todolist.entity.Record;
 import com.todolist.entity.RecordStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class RecordDao {
-    private final List<Record> records = new ArrayList<>(
-            Arrays.asList(
-                    new Record("Take a shower", RecordStatus.ACTIVE),
-                    new Record("Buy flowers", RecordStatus.DONE),
-                    new Record("Go to the gym", RecordStatus.ACTIVE)
-            ));
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    public RecordDao(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
 
     public List<Record> findALlRecords(){
-        return new ArrayList<>(records);
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        try {
+            entityManager.getTransaction().begin();
+
+            Query query = entityManager.createQuery("SELECT r FROM Record r");
+            List<Record> records = query.getResultList();
+
+            entityManager.getTransaction().commit();
+            return records;
+        }catch (Exception exception){
+            exception.printStackTrace();
+            entityManager.getTransaction().rollback();
+            return Collections.emptyList();
+        }finally {
+            entityManager.close();
+        }
+
     }
 
     public void saveRecord(Record record){
